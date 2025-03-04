610274df890e6e1ee042e3cbcf17227e6d4ba28b
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-03-04 22:03:19 +0300

    ui improvement

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -8,21 +8,21 @@ function ProjectsPage() {
         <div className="projects-page-container">
             <div className="two-card-row first-row">
                 <div className="card">
+                    <div className="title">
+                        <h3>YTÜ Satranç Robotu</h3>
+                    </div>
                     <img src={ytuChessRobot} alt="Logo"/>
                     <div className="explanation">
-                        <div className="title">
-                            <h3>YTÜ Satranç Robotu</h3>
-                        </div>
                         <p>* Frontend, backend ve tahta algoritması ekibinde görev alıyorum.</p>
                         <p>* Kodları incelemek için <a href="https://github.com/YTU-Satranc-Kulubu" target="_blank">YTÜ Satranç Robotu</a>'na göz atabilirsiniz</p>
                     </div>          
                 </div>
                 <div className="card">
+                    <div className="title">
+                        <h3>Demirelli Rent Car</h3>
+                    </div>
                     <img src={demirelliRentCar} alt="Logo"/>
                     <div className="explanation">
-                        <div className="title">
-                            <h3>Demirelli Rent Car</h3>
-                        </div>
                         <p>* Staj projem olarak tamamladım.</p>
                         <p>* Elden teslim araç kiralama işlemi yapan bir işletmenin işini görecek bir sistem kodladım.</p>
                         <p>* Kullanılan Teknolojiler: Angular, .Net ve MsSQL</p>
