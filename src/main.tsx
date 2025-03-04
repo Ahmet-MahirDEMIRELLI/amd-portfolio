94581cfe2f98edb47c5e66a0be5975d95aecdda8
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-03-04 19:28:50 +0300

    experience-page added

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import './index.css'
 import App from './App.tsx'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import MainPage from './components/MainPage.tsx'
+import ExperiencePage from './components/ExperiencePage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<MainPage />} />
-        {/*<Route path="about" element={<MainPage />} />*/}
+        <Route path="experience" element={<ExperiencePage />} />
       </Route>
     </Routes>
   </HashRouter>,
